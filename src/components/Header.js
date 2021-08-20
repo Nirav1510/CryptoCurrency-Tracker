0cbc52f511079385c4fd3f35df5e62057534be4f
Nirav <gusainirav1@gmail.com>
2021-08-21 00:00:12 +0530

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-center text-3xl text-yellow-500 mt-3 mb-4">
+      Coin Tracker
+    </div>
+  );
+};
+
+export default Header;
