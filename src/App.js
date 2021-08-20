8833aac329669fb416d6647ce5dfe9b56061cd5f
Nirav <gusainirav1@gmail.com>
2021-08-21 00:00:52 +0530

    Initial Change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import CoinDetailPage from './Pages/CoinDetailPage'
 import CoinSummaryPage from './Pages/CoinSummaryPage'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={CoinSummaryPage} />
+    </BrowserRouter>
   )
 }
 
