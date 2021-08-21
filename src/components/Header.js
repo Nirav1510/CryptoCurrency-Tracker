a5a73c06a3fe326dc9794f21b7a43a7b3e5d9a50
Nirav <gusainirav1@gmail.com>
2021-08-21 22:54:12 +0530

    Header Complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="text-center text-3xl text-yellow-500 mt-3 mb-4">
+    <div className="text-center text-4xl text-yellow-500 mt-3 mb-4 font-bold italic">
       Coin Tracker
     </div>
   );
