9b4c4b4cf74c65e9f187528c0e2ddc6d96dea259
Nirav <gusainirav1@gmail.com>
2021-08-26 16:58:34 +0530

    Coin rendered on screen successfully

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -7,7 +7,7 @@ const Coin = ({ coin }) => {
       <li className="coinlist-item list-group-item list-group-item-action d-flex justify-content-between align-items-center text-dark">
         <img className="coinlist-image" src={coin.image} alt="" />
         <span className="text-decoration-none">{coin.current_price}</span>
-
+        
         <span
           className={
             coin.price_change_percentage_24h < 0
@@ -23,7 +23,9 @@ const Coin = ({ coin }) => {
           )}
           {coin.price_change_percentage_24h}
         </span>
+
         <i className="delete-icon far fa-times-circle text-danger"></i>
+      
       </li>
     </Link>
   );
