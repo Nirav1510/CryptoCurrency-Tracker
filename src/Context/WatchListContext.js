60f0df181d16cda7811358864cd2430f4e038db1
Nirav <gusainirav1@gmail.com>
2021-08-26 17:34:39 +0530

    delete function

--- src/Context/WatchListContext.js
+++ src/Context/WatchListContext.js
@@ -5,7 +5,13 @@ export const WatchListContext = createContext();
 export const WatchListContextProvider = (props) => {
   const [watchList, setWatchList] = useState(["bitcoin", "ethereum", "ripple"]);
 
-  const deleteCoin = (coin) => {};
+  const deleteCoin = (coin) => {
+    setWatchList(
+      watchList.filter((ele) => {
+        return ele !== coin;
+      })
+    );
+  };
 
   return (
     <WatchListContext.Provider value={{ watchList, deleteCoin }}>
