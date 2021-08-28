22f5526ed88bb0cdb91104fa600158f0b6736759
Nirav <gusainirav1@gmail.com>
2021-08-28 18:28:02 +0530

    Working on chart

--- src/components/HistoryChart.js
+++ src/components/HistoryChart.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import Chartjs from "chart.js";
 
 const HistoryChart = () => {
-    return (
-        <div>
-            Chat
-        </div>
-    )
-}
+  const chartRef = useRef(); //accesing DOM element in React
 
-export default HistoryChart
+  useEffect(() => {
+    if (chartRef && chartRef.current) {
+      const chartInstance = new Chartjs(chartRef.current, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    }
+  }, []);
+  return (
+    <div className="bg-white border mt-2 rounded p-3">
+        <div></div>
+      <div>
+        <canvas id="myChart" ref={chartRef} width={250} height={250}></canvas>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryChart;
