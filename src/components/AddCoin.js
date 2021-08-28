d7a28d3a09d86864cc05b13d94ce4f0b57062499
Nirav <gusainirav1@gmail.com>
2021-08-29 00:33:00 +0530

    Dropdown created

--- src/components/AddCoin.js
+++ src/components/AddCoin.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { WatchListContext } from "../Context/WatchListContext";
 
 const AddCoin = () => {
+  const [isActive, setIsActive] = useState(false);
+  const { addCoin } = useContext(WatchListContext);
 
-    const availableCoins = [
-        "bitcoin",
-        "ethereum",
-        "ripple",
-        "tether",
-        "bitcoin-cash",
-        "litecoin",
-        "eos",
-        "okb",
-        "tezos",
-        "cardano",
-      ];
-      
-    return (
-        <div>
-            Add coin
-        </div>
-    )
-}
+  const availableCoins = [
+    "bitcoin",
+    "ethereum",
+    "ripple",
+    "tether",
+    "bitcoin-cash",
+    "litecoin",
+    "eos",
+    "okb",
+    "tezos",
+    "cardano",
+  ];
 
-export default AddCoin
+  const handleClick = (coin) => {
+    addCoin(coin);
+    setIsActive(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className="btn btn-primary dropdown-toggle"
+        type="button"
+      >
+        Add Coin
+      </button>
+      <div className={isActive ? "dropdown-menu show" : "dropdown-menu"}>
+        {availableCoins.map((el) => {
+          return (
+            <a
+              onClick={() => handleClick(el)}
+              href="#"
+              className="dropdown-item"
+            >
+              {el}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AddCoin;
